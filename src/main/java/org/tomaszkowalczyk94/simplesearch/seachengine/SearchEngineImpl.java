9b691c3895a7321a192b643856b5309b6e06ff82
tomaszkowalczyk94 <tomasz.kowalczyk94@gmail.com>
2020-10-01 22:08:54 +0200

    simple test for search engine and small changes in interfaces

--- src/main/java/org/tomaszkowalczyk94/simplesearch/seachengine/SearchEngineImpl.java
+++ src/main/java/org/tomaszkowalczyk94/simplesearch/seachengine/SearchEngineImpl.java
@@ -1,13 +1,15 @@
 package org.tomaszkowalczyk94.simplesearch.seachengine;
 
 import org.tomaszkowalczyk94.simplesearch.memory.Memory;
-import org.tomaszkowalczyk94.simplesearch.memory.Word;
 
 import java.util.List;
 
 class SearchEngineImpl implements SearchEngine{
+
     @Override
-    public void search(List<Word> words, Memory memory) {
+    public List<SearchResult> search(List<String> findBy, Memory memory) {
+
+        return null;
 
     }
 }
